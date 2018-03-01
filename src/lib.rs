4100aba388dccf501dde089f1f6b2e6f7dc55006
chris13524 <chris@hichris.com>
2018-02-28 21:13:36 -0500

    Panics will now print out their messages. Now using nom for Mish parsing.

--- src/lib.rs
+++ src/lib.rs
@@ -7,11 +7,22 @@
 
 extern crate projectasiago_feta;
 
+extern crate core as std;
+
+#[macro_use]
+extern crate nom;
+
+named!(alt_tags, alt!(tag!("abcd") | tag!("efgh")));
+
 ///```
 /// let result = projectasiago_mish::hello_world();
 /// assert_eq!(result, "hi world fjdskl");
 ///```
 pub fn hello_world() -> &'static str {
+	assert_eq!(alt_tags(b"abcdxxx"), Ok((&b"xxx"[..], &b"abcd"[..])));
+	assert_eq!(alt_tags(b"efghxxx"), Ok((&b"xxx"[..], &b"efgh"[..])));
+	assert_eq!(alt_tags(b"ijklxxx"), Err(nom::Err::Error(error_position!(&b"ijklxxx"[..], nom::ErrorKind::Alt))));
+	
 	let s: &'static str = "hi world fjdskl";
 	return s;
 }
