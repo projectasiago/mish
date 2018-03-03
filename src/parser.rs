0dd41e4301cf60b82f683f3c307670db86fa53b1
chris13524 <chris@hichris.com>
2018-03-03 15:47:36 -0500

    Expressions now allow whitespace.

--- src/parser.rs
+++ src/parser.rs
@@ -1,8 +1,6 @@
 #![allow(unused_variables)]
 #![allow(dead_code)]
 
-use nom::Err::Error;
-use nom::ErrorKind;
 use std::vec::Vec;
 
 named!(digit<char>, one_of!(&b"0123456789"[..]));
@@ -21,21 +19,26 @@ named!(expression<Expression>, alt!(
 	decnumber => { |number: Vec<char>| Expression::DecimalNumber(number) } |
 	subexpression => { |expression: Expression| expression }
 ));
-named!(subexpression<Expression>, delimited!(char!('('), expression, char!(')')));
+named!(subexpression<Expression>, ws!(delimited!(char!('('), expression, char!(')'))));
 
 #[test]
 pub fn test() {
+	use nom::Err::Error;
+	use nom::ErrorKind;
+	
 	// digits
 	assert_eq!(digit(b"1"), Ok((&b""[..], '1')));
 	assert_eq!(digit(b"a"), Err(Error(error_position!(&b"a"[..], ErrorKind::OneOf))));
-	assert_eq!(decnumber(b"12 "), Ok((&b" "[..], vec!['1', '2'])));
-	assert_eq!(decnumber(b"a "), Err(Error(error_position!(&b"a "[..], ErrorKind::Many1))));
+	assert_eq!(decnumber(b"12x"), Ok((&b"x"[..], vec!['1', '2'])));
+	assert_eq!(decnumber(b"1 2x"), Ok((&b" 2x"[..], vec!['1'])));
+	assert_eq!(decnumber(b"ax"), Err(Error(error_position!(&b"ax"[..], ErrorKind::Many1))));
 	
 	// letters
 	assert_eq!(letter(b"a"), Ok((&b""[..], 'a')));
 	assert_eq!(letter(b"0"), Err(Error(error_position!(&b"0"[..], ErrorKind::Alt))));
 	
 	// expression
-	assert_eq!(expression(b"12 "), Ok((&b" "[..], Expression::DecimalNumber(vec!['1', '2']))));
-	assert_eq!(expression(b"(12) "), Ok((&b" "[..], Expression::DecimalNumber(vec!['1', '2']))));
+	assert_eq!(expression(b"12x"), Ok((&b"x"[..], Expression::DecimalNumber(vec!['1', '2']))));
+	assert_eq!(expression(b"(12)x"), Ok((&b"x"[..], Expression::DecimalNumber(vec!['1', '2']))));
+	assert_eq!(expression(b" ( 12 ) x"), Ok((&b"x"[..], Expression::DecimalNumber(vec!['1', '2']))));
 }
